847351816991809be909e9565e7abecb88e2d6ae
Dario-Hesami <Dario.Academic@gmail.com>
2023-09-21 10:37:12 -0400

    Week 3 - completed

--- DotNetBookstore/Controllers/CategoriesController.cs
+++ DotNetBookstore/Controllers/CategoriesController.cs
@@ -1,17 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DotNetBookstore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetBookstore.Controllers
 {
     public class CategoriesController : Controller
     {
         public IActionResult Index()
+
         {
-            return View();
+            // use the Category model to generate 10 categories in memory for display in the view
+            var categories = new List<Category>();
+            for (int i = 0; i< 11; i++)
+            {
+                categories.Add(new Category { CategoryId = i, Name = "Category " + i.ToString() });
+            }
+            return View(categories);
         }
         public IActionResult Browse(string category)
 
         {
+            if (category == null)
+            {
+                return RedirectToAction("index");
+            }
+
             ViewBag.category = category;
+            return View();
+        }
+        public IActionResult Create()
+        {
             return View();
         }
     }
